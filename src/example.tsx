a24f0e0d125c364e5e38974d25b29f60059c6cc4
Aubrey Quinn <80953505+aubreyquinn@users.noreply.github.com>
2022-10-19 11:59:19 +0100

    Initial commit

---
+++ src/example.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@fluentui/react-components";
+import * as React from 'react';
+export const Appearance = () => <>
+    <Button>Default</Button>
+    <Button appearance="primary">Primary</Button>
+    <Button appearance="outline">Outline</Button>
+    <Button appearance="subtle">Subtle</Button>
+    <Button appearance="transparent">Transparent</Button>
+  </>;
+Appearance.parameters = {
+  docs: {
+    description: {
+      story: '- `(undefined)`: the button appears with the default style\n' + '- `primary`: emphasizes the button as a primary action.\n' + '- `outline`: removes background styling.\n' + '- `subtle`: minimizes emphasis to blend into the background until hovered or focused\n' + '- `transparent`: removes background and border styling.\n'
+    }
+  }
+};
